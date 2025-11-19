29f3bb0b082926eb23d53e51c7e5706271081d7a
LauraBailly08 <laura.bailly02@edu.univ-fcomte.fr>
2025-11-19 09:56:03 +0100

    button scroll

--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,20 @@
 // *** CAROUSEL ***
 // TODO
+
+
+const carousel = document.querySelector(".carousel__container");;
+const prevButton = document.querySelector(".carousel__button--prev");
+const nextButton = document.querySelector(".carousel__button--next");
+
+// Largeur de défilement d’un item
+if (carousel) {
+    // Scroll au clic sur le bouton précédent
+    nextButton.addEventListener("click", () => {
+        carousel.scrollLeft += 300;
+    });
+
+    // Scroll au clic sur le bouton suivant
+    prevButton.addEventListener("click", () => {
+        carousel.scrollLeft -= 300;
+    });
+}
